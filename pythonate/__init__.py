5eeabf00f631ac36296910720bebca322ac7903d
Nate <nathan.harris@force5solutions.com>
2020-10-15 14:46:42 -0400

    Change requirements, deactivate SQL for now

--- pythonate/__init__.py
+++ pythonate/__init__.py
@@ -1,4 +1,4 @@
 from ._version import __author__, __version__
 
 from pythonate import analytics, dictionary, dropbox, encryption, files, \
-    general, logs, pastebin, random, requests, sorting, sql, time
+    general, logs, pastebin, random, requests, sorting, time
