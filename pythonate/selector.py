c5e092a9650337bec2514ece8f744c47276387eb
nwithan8 <nwithan8@users.noreply.github.com>
2023-08-08 23:44:38 -0600

    - Add option/options collection for managing aliases

---
+++ pythonate/selector.py
@@ -0,0 +1,45 @@
+from typing import Any, Union
+
+
+class Option:
+    """
+    An option is a name-value pair with a list of aliases.
+    """
+
+    def __init__(self, name: str, value: Any, aliases: list[str] = None):
+        """
+        :param name: The name of the option.
+        :type name: str
+        :param value: The value of the option.
+        :type value: Any
+        :param aliases: A list of aliases for the option.
+        :type aliases: list[str]
+        """
+        self.name = name
+        self.value = value
+        self.aliases = aliases or []
+
+
+class Options:
+    """
+    A collection of options.
+    """
+
+    def __init__(self, options: list[Option]):
+        """
+        :param options: A list of options.
+        :type options: list[Option]
+        """
+        self.options = options
+
+    def match(self, name: str) -> Union[Option, None]:
+        """
+        :param name: The name or alias of an option.
+        :type name: str
+        :return: The option matching the provided name or alias, or None.
+        :rtype: Option | None
+        """
+        for option in self.options:
+            if name == option.name or name in option.aliases:
+                return option
+        return None
